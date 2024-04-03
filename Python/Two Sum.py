d1b53ea3d99c51b0fb2d0ef97b4fc6e68eb88d02
Ryan <ryankapansky@gmail.com>
2024-04-03 14:24:22 +0300

    Two Sum Python parameters requirments were added

--- Python/Two Sum.py
+++ Python/Two Sum.py
@@ -1,16 +1,18 @@
 #Link ref https://leetcode.com/problems/two-sum/
 
+from typing import List
+
 class Solution:
-    def twosum(numbers, target):
+    def twosum(numbers: list[int], target: int) -> list[int]:
         numbers_length = len(numbers)
         for i in range(numbers_length - 1):
             for j in range(i + 1, numbers_length):
                 if numbers[i] + numbers[j] == target:
                     return [i, j]
         return []
-                
 
-numbers = [3,3]
-target = 6
+
+numbers = [2,7,11,15]
+target = 9
 test = Solution.twosum(numbers, target)
 print(test)
